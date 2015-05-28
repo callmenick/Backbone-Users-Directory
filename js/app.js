645a838383b1b6d1bfe64ede722a3d13e3fda321
Nick Salloum <nick@callmenick.com>
2015-05-27 23:07:26 -0400

    repurposed app, and sort functionality working

--- js/app.js
+++ js/app.js
@@ -1,80 +1,124 @@
-(function() {
+/**
+ * The User model
+ * 
+ * This is how we will model an individual user in our table, with the defaults
+ * as blanks.
+ */
+var User = Backbone.Model.extend({
+  defaults: {
+    firstname: '',
+    lastname: '',
+    email: ''
+  }
+});
 
-  /**
-   * The model
-   */
 
-  var UsersModel = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        firstname: "",
-        lastname: "",
-        email: ""
-      }
-    }
-  });
 
-  /**
-   * The collection
-   */
 
-  var UsersCollection = Backbone.Collection.extend({
-    model: UsersModel,
-    comparator: "firstname",
-    url: "assets/users.json"
-  });
 
-  /*
-
-  var testCollection = new UsersCollection();
-
-  testCollection.fetch({
-    success: function() {
-      console.log(testCollection);
-    }
-  });
-
-  */
-
-  /**
-   * The users view
-   */
-
-  var UsersView = Backbone.View.extend({
-    el: "#users__body",
-
-    collection: new UsersCollection(),
-
-    initialize: function() {
-      var scope = this;
-      this.collection.fetch({
-        success: function() {
-          scope.render();
-        }
-      });
-    },
-
-    render: function() {
-      var scope = this;
-      this.collection.forEach(function(model) {
-        scope.output(model);
-      });
-      return this;
-    },
-
-    output: function(model) {
-      var row = document.createElement("tr");
-      row.innerHTML = "<td>" + model.get("firstname") + "</td>\
-                       <td>" + model.get("lastname") + "</td>\
-                       <td>" + model.get("email") + "</td>";
-      this.el.appendChild(row);
-    }
-  });
-
-  /**
-   * Render app
-   */
+/**
+ * The Users collection
+ *
+ * The users collection is a collection of our users based on the User model,
+ * and the collection, for the sake of this demo, is retrieved from a generated
+ * json list in the assets directory.
+ */
+var UsersCollection = Backbone.Collection.extend({
   
-  var app = new UsersView();
+  model: User,
 
-})();
+  url: 'assets/users.json',
+
+  comparator: function(model) {
+    return model.get('firstname').toLowerCase();
+  }
+
+});
+
+var Users = new UsersCollection();
+
+
+
+
+
+/**
+ * The Users list
+ *
+ * The users list is a view that contains all the individual user views. It is,
+ * in essence, a parent view for al the users.
+ */
+var UsersView = Backbone.View.extend({
+
+  el: '#users',
+
+  events: {
+    'change #sort': 'sortCollection'
+  },
+
+  initialize: function() {
+    this.listenTo(Users, 'sort', this.render);
+    Users.fetch();
+  },
+
+  render: function() {
+    this.$('#users-table__body').html('');
+
+    Users.forEach(function(model) {
+      var user = new UserView({
+        model: model
+      });
+      $('#users-table__body').append(user.render().el);
+    });
+
+    return this;
+  },
+
+  sortCollection: function(e) {
+    console.log(e.target.value);
+    Users.comparator = e.target.value;
+    Users.sort();
+  }
+
+});
+
+
+
+
+
+/**
+ * The User view
+ *
+ * The user view is created for each user so that we can add more functionality
+ * to each user later on.
+ */
+var UserView = Backbone.View.extend({
+
+  tagName: 'tr',
+
+  template: _.template($('#user-template').html()),
+
+  events: {
+    'click .users__action--delete': 'deleteUser'
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+
+  deleteUser: function(e) {
+    e.preventDefault();
+    console.log('delete ' + this.model.get('firstname'));
+  }
+
+});
+
+
+
+
+
+/**
+ * Instantiate the users list which populates the table and gets ready for
+ * the action.
+ */
+new UsersView();
